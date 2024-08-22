59d84bc6c1cda44af31869d40eb9fc7aa936f815
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2024-08-22 20:51:36 +0200

    Refactor the dark theme along the lines of official themes.

--- falconry_pygments_theme/dark.py
+++ falconry_pygments_theme/dark.py
@@ -18,20 +18,14 @@ from pygments.token import (
     Error,
     Generic,
     Keyword,
-    Literal,
     Name,
     Number,
     Operator,
-    Other,
-    Punctuation,
     String,
-    Text,
+    Token,
 )
 
-
-class FontStyle:
-    bold = 'bold'
-    italic = 'italic'
+from .common import bold, italic, bold_italic
 
 
 class Colors:
@@ -40,83 +34,56 @@ class Colors:
     crimson = '#dc143c'
     brown = '#bc9458'
     green = '#a5c261'
+    pear = '#c9cc3f'
     blue = '#d0d0ff'
-    darkgray = '#202020'
+    darkgray = '#171717'
     white = '#e6e1dc'
 
 
-class DarkTheme(Style):
+class FalconryDarkStyle(Style):
     """Falconry dark Pygments style."""
 
-    default_style = ''
+    name = 'falconry-dark'
 
     background_color = Colors.darkgray
-    highlight_color = '#ffd9002e'
+    highlight_color = Colors.white
 
     styles = {
-        Text: Colors.white,  # class:  ''
-        Error: Colors.crimson,  # class: 'err'
-        Other: '',  # class 'x'
-        Comment: FontStyle.italic + ' ' + Colors.brown,  # class: 'c'
-        Keyword: Colors.orange,  # class: 'k'
-        Keyword.Constant: Colors.orange,  # class: 'kc'
-        # Keyword.Declaration:       "",            # class: 'kd'
-        # Keyword.Namespace:         "",            # class: 'kn'
-        # Keyword.Pseudo:            "",            # class: 'kp'
-        # Keyword.Reserved:          "",            # class: 'kr'
-        Keyword.Type: Colors.orange,  # class: 'kt'
-        Operator: Colors.green,  # class: 'o'
-        Operator.Word: Colors.white,  # class: 'ow'
-        Punctuation: Colors.white,  # class: 'p'
-        Name: Colors.white,  # class: 'n'
-        Name.Attribute: Colors.yellow,  # class: 'na'
-        Name.Builtin: Colors.orange,  # class: 'nb'
-        Name.Builtin.Pseudo: Colors.orange,  # class: 'bp'
-        Name.Class: Colors.yellow,  # class: 'nc'
-        Name.Constant: Colors.blue,  # class: 'no'
-        Name.Decorator: Colors.orange,  # class: 'nd'
-        Name.Entity: Colors.green,  # class: 'ni'
-        Name.Exception: Colors.crimson,  # class: 'ne'
-        Name.Function: Colors.yellow,  # class: 'nf'
-        Name.Property: Colors.white,  # class: 'py'
-        Name.Label: Colors.white,  # class: 'nl'
-        Name.Namespace: Colors.white,  # class: 'nn'
-        # Name.Other:                "",            # class: 'nx'
-        Name.Tag: Colors.blue,  # class: 'nt'
-        Name.Variable: Colors.white,  # class: 'nv'
-        Name.Variable.Magic: Colors.orange,
-        # Name.Variable.Class:       "",            # class: 'vc'
-        # Name.Variable.Global:      "",            # class: 'vg'
-        # Name.Variable.Instance:    "",            # class: 'vi'
-        Number: Colors.orange,  # class: 'm'
-        # Number.Float:              "",            # class: 'mf'
-        # Number.Hex:                "",            # class: 'mh'
-        # Number.Integer:            "",            # class: 'mi'
-        # Number.Integer.Long:       "",            # class: 'il'
-        # Number.Oct:                "",            # class: 'mo'
-        Literal: Colors.blue,  # class: 'l'
-        # Literal.Date:              "",            # class: 'ld'
-        String: Colors.green,  # class: 's'
-        String.Backtick: Colors.green,  # class: 'sb'
-        # String.Char:               "",            # class: 'sc'
-        # String.Doc:                "",            # class: 'sd'
-        # String.Double:             "",            # class: 's2'
-        # String.Escape:             "",            # class: 'se'
-        # String.Heredoc:            "",            # class: 'sh'
-        # String.Interpol:           "",            # class: 'si'
-        # String.Other:              "",            # class: 'sx'
-        String.Regex: Colors.crimson,  # class: 'sr'
-        # String.Single:             "",            # class: 's1'
-        String.Symbol: Colors.blue,  # class: 'ss'
-        # Generic:                   "",            # class: 'g'
-        Generic.Deleted: Colors.crimson,  # class: 'gd',
-        Generic.Emph: FontStyle.italic,  # class: 'ge'
-        # Generic.Error:             "",            # class: 'gr'
-        Generic.Heading: Colors.yellow,  # class: 'gh'
-        Generic.Subheading: Colors.blue,  # class: 'gu'
-        # Generic.Inserted:          "",            # class: 'gi'
-        # Generic.Output:            "",            # class: 'go'
-        # Generic.Prompt:            "",            # class: 'gp'
-        Generic.Strong: FontStyle.bold,  # class: 'gs'
-        # Generic.Traceback:         "",            # class: 'gt'
+        Token: Colors.white,
+        Comment: italic | Colors.brown,
+        Comment.PreProc: italic | Colors.brown,
+        Comment.Special: bold_italic | Colors.brown,
+        Keyword: bold | Colors.orange,
+        Keyword.Type: Colors.blue,
+        Operator: Colors.white,
+        Operator.Word: Colors.white,
+        String: Colors.green,
+        String.Escape: Colors.pear,
+        Number: Colors.pear,
+        Name.Builtin: bold | Colors.orange,
+        Name.Builtin.Pseudo: Colors.blue,
+        Name.Variable: Colors.white,
+        Name.Variable.Magic: Colors.blue,
+        Name.Constant: Colors.green,
+        Name.Class: bold | Colors.yellow,
+        Name.Function: bold | Colors.yellow,
+        Name.Namespace: Colors.white,
+        Name.Exception: Colors.crimson,
+        Name.Tag: Colors.white,
+        Name.Attribute: Colors.white,
+        Name.Decorator: bold | Colors.blue,
+        Generic.Heading: bold | Colors.yellow,
+        Generic.Subheading: bold | Colors.yellow,
+        Generic.Deleted: Colors.crimson,
+        Generic.Inserted: Colors.blue,
+        Generic.Error: Colors.crimson,
+        Generic.Emph: italic,
+        Generic.Strong: bold,
+        Generic.Prompt: Colors.green,
+        Generic.Output: Colors.yellow,
+        Generic.Traceback: Colors.crimson,
+        Error: Colors.crimson,
     }
+
+
+__all__ = ['FalconryDarkStyle']
