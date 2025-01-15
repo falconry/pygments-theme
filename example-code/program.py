e36d17c1e78b3faa022551ac8f83a522b369d56d
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2025-01-15 21:34:16 +0100

    Style the forgotten Operator.Word; make exceptions bold.

--- example-code/program.py
+++ example-code/program.py
@@ -10,6 +10,21 @@ class ExampleClass:
 
     def __init__(self, item):
         self._item = item
+        # Just to check formatting of some boolean operators
+        self._bools = (False or True, False and True)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if exc_value is not None:
+            print(f'Exception was raised: {exc_type.__name__}: {exc_value}')
+
+    @classmethod
+    def example_context(cls, number):
+        with cls(frozenset({1, 2, 3})) as example:
+            if number not in example._item:
+                pass
 
     @passthrough
     def print_item(self):
@@ -28,6 +43,8 @@ def main():
     example = ExampleClass([1, 2, 3])
     example.print_item()
     example.try_except()
+
+    ExampleClass.example_context(4)
 
 
 # NOTE: __name__ is rendered as the class Name.Variable.Magic in Pygments.
